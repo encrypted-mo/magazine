9f425a068a669848cf65071f2372dd3e298061e9
encrypted-mo <fl.festusliberty@gmail.com>
2026-09-17 02:13:37 +0000

    Update Footer.tsx

--- components/common/Footer/Footer.tsx
+++ components/common/Footer/Footer.tsx
@@ -2,9 +2,6 @@ import Link from 'next/link'
 import SocialUrls from './SocialUrls'
 import ThemeSwitch from '../ThemeSwitch'
 import s from './Footer.module.css'
-import ExternalLink from '@components/ui/Link/ExternalLink'
-import Github from '@components/icons/Github'
-
 const Footer = ({ categories, pages }: TNavigation) => {
   return (
     <footer className="block bottom-0 left-0 right-0 bg-primary-05 px-6 py-6 md:px-32 lg:px-48 xl:px-1/5">
@@ -24,7 +21,6 @@ const Footer = ({ categories, pages }: TNavigation) => {
             ))}
           </ul>
         </div>
-
         <div>
           <h3 className={s.heading}>About</h3>
           <ul className={s.ul}>
@@ -33,7 +29,6 @@ const Footer = ({ categories, pages }: TNavigation) => {
                 <a className={s.link}>Contributors</a>
               </Link>
             </li>
-
             <li>
               <Link href="/contributors">
                 <a className={s.link}>Contact</a>
@@ -41,7 +36,6 @@ const Footer = ({ categories, pages }: TNavigation) => {
             </li>
           </ul>
         </div>
-
         <div>
           <h3 className={s.heading}>Other</h3>
           <ul className={s.ul}>
@@ -55,23 +49,9 @@ const Footer = ({ categories, pages }: TNavigation) => {
           </ul>
         </div>
       </nav>
-
       <SocialUrls />
-
       <ThemeSwitch />
-
-      <ExternalLink
-        to="https://github.com/edgarlr/magazine"
-        ariaLabel="Link to source coude"
-        className="mx-auto flex items-center w-max pt-4 py-2 opacity-80 hover:opacity-100 text-sm"
-      >
-        <span className="mr-2">
-          <Github width="16" height="16" />
-        </span>
-        Source
-      </ExternalLink>
     </footer>
   )
 }
-
 export default Footer
