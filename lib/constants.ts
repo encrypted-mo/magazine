9e3fd995361c1f2ccbc3c3cbb131c966639f6cf4
encrypted-mo <fl.festusliberty@gmail.com>
2026-09-17 02:01:57 +0000

    Modify social usernames in constants
    Updated social media usernames for better accuracy.

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,20 +1,24 @@
 // social base url
 export const TwitterURL = 'https://twitter.com'
+
 // Global Data
 export const SITE_URL = 'https://magazine-starter.vercel.app'
 export const SITE_NAME = 'Magazine'
 export const SITE_LOGO =
   'https://res.cloudinary.com/dliiwavlg/image/upload/v1615590785/magazineog_qppjhg.jpg'
+
 export const SOCIAL_USERNAMES = {
-  twitter: 'edgarlr_',
-  instagram: 'edgar.lr',
-  facebook: 'edgarlr',
-  youtube: null,
-  linkedin: 'edgarlr',
+  twitter: null,
+  instagram: 'highskul_creatives',
+  facebook: '100094129848013',
+  youtube: 'Highskul_creatives',
+  linkedin: null,
 }
+
 // Default SEO
 export const SEO_DESCRIPTION =
   'All-in-one  open source digital magazine starter kit with high performance and fully customizable. '
+
 export const OG_IMAGE = {
   large: {
     url:
