d7acc01e88a5addd79bc70cd21b2e87931242c28
encrypted-mo <fl.festusliberty@gmail.com>
2026-09-17 01:21:09 +0000

    Refactor constants for site and social media details
    Updated social media URLs and site information.

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,23 +1,23 @@
-// social base url
+// Social base URL
 export const TwitterURL = 'https://twitter.com'
 
 // Global Data
-export const SITE_URL = 'https://magazine-starter.vercel.app'
-export const SITE_NAME = 'Magazine'
+export const SITE_URL = 'https://magazine-rouge.vercel.app'
+export const SITE_NAME = 'HighSkul Creatives'
 export const SITE_LOGO =
   'https://res.cloudinary.com/dliiwavlg/image/upload/v1615590785/magazineog_qppjhg.jpg'
 
 export const SOCIAL_USERNAMES = {
-  twitter: 'edgarlr_',
-  instagram: 'edgar.lr',
-  facebook: 'edgarlr',
-  youtube: null,
-  linkedin: 'edgarlr',
+  twitter: null,
+  instagram: 'https://www.instagram.com/highskul_creatives/',
+  facebook: 'https://www.facebook.com/profile.php?id=100094129848013',
+  youtube: 'https://www.youtube.com/@Highskul_creatives',
+  linkedin: null,
 }
 
 // Default SEO
 export const SEO_DESCRIPTION =
-  'All-in-one  open source digital magazine starter kit with high performance and fully customizable. '
+  'HighSkul Creatives is a youth-focused digital publication covering education, student life, campus opinions, news, opportunities and careers.'
 
 export const OG_IMAGE = {
   large: {
