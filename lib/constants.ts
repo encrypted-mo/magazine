c7d48057ad430e4a29219def343fd5680bdc6b58
encrypted-mo <fl.festusliberty@gmail.com>
2026-09-17 01:40:48 +0000

    Refactor social media constants in constants.ts
    Updated social media URLs and cleaned up constants.

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,24 +1,20 @@
-// Social base URL
+// social base url
 export const TwitterURL = 'https://twitter.com'
-
 // Global Data
 export const SITE_URL = 'https://magazine-rouge.vercel.app'
 export const SITE_NAME = 'HighSkul Creatives'
 export const SITE_LOGO =
   'https://res.cloudinary.com/dliiwavlg/image/upload/v1615590785/magazineog_qppjhg.jpg'
-
 export const SOCIAL_USERNAMES = {
   twitter: null,
-  instagram: 'https://www.instagram.com/highskul_creatives/',
-  facebook: 'https://www.facebook.com/profile.php?id=100094129848013',
-  youtube: 'https://www.youtube.com/@Highskul_creatives',
+  instagram: 'highskul_creatives',
+  facebook: null,
+  youtube: null,
   linkedin: null,
 }
-
 // Default SEO
 export const SEO_DESCRIPTION =
   'HighSkul Creatives is a youth-focused digital publication covering education, student life, campus opinions, news, opportunities and careers.'
-
 export const OG_IMAGE = {
   large: {
     url:
