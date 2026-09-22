2664d3a0267e2881a5586eaf622a573050ac6a22
encrypted-mo <fl.festusliberty@gmail.com>
2026-09-22 03:19:57 +0000

    Update _document.tsx for GA and favicon setup
    Refactor Google Analytics script inclusion for production environment and add favicon links.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,33 +12,50 @@ class MyDocument extends Document {
 
     return initialProps
   }
+
   render() {
     return (
       <Html lang="en">
-        {process.env.NODE_ENV === 'production' ? (
-          // Only run GA on Production
-          <Head>
-            <script
-              async
-              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}
-            />
-            <script
-              // eslint-disable-next-line react/no-danger
-              dangerouslySetInnerHTML={{
-                __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${process.env.GA_MEASUREMENT_ID}', {
-                  page_path: window.location.pathname,
-                });
-              `,
-              }}
-            />
-          </Head>
-        ) : (
-          <Head />
-        )}
+        <Head>
+          {/* HighSkul Creatives favicon system */}
+          <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+          <link rel="icon" href="/favicon.ico" sizes="any" />
+          <link
+            rel="icon"
+            href="/favicon-96x96.png"
+            sizes="96x96"
+            type="image/png"
+          />
+          <link
+            rel="apple-touch-icon"
+            href="/apple-touch-icon.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+
+          {process.env.NODE_ENV === 'production' && (
+            <>
+              {/* Only run GA on Production */}
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}
+              />
+              <script
+                // eslint-disable-next-line react/no-danger
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.GA_MEASUREMENT_ID}', {
+                      page_path: window.location.pathname,
+                    });
+                  `,
+                }}
+              />
+            </>
+          )}
+        </Head>
+
         <body>
           <Main />
           <NextScript />
