b98ac3fe9cdcbfd68bf9074d981f5ceb0b68db7f
encrypted-mo <fl.festusliberty@gmail.com>
2026-09-22 04:09:09 +0000

    Refactor Head component for production and development

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,56 +5,47 @@ import Document, {
   NextScript,
   DocumentContext,
 } from 'next/document'
-
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx)
-
     return initialProps
   }
-
   render() {
     return (
       <Html lang="en">
-        <Head>
-          {/* HighSkul Creatives favicon system */}
-          <link rel="icon" href="/favicon.ico" sizes="any" />
-          <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png" />
-          <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png" />
-          <link
-            rel="icon"
-            href="/dark-32x32.png"
-            sizes="32x32"
-            type="image/png"
-            media="(prefers-color-scheme: dark)"
-          />
-          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#f5a623" />
-
-          {process.env.NODE_ENV === 'production' && (
-            <>
-              {/* Only run GA on Production */}
-              <script
-                async
-                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}
-              />
-              <script
-                // eslint-disable-next-line react/no-danger
-                dangerouslySetInnerHTML={{
-                  __html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${process.env.GA_MEASUREMENT_ID}', {
-                      page_path: window.location.pathname,
-                    });
-                  `,
-                }}
-              />
-            </>
-          )}
-        </Head>
-
+        {process.env.NODE_ENV === 'production' ? (
+          // Only run GA on Production
+          <Head>
+            <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+            <link rel="icon" href="/favicon.ico" sizes="any" />
+            <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+            <link rel="manifest" href="/site.webmanifest" />
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}
+            />
+            <script
+              // eslint-disable-next-line react/no-danger
+              dangerouslySetInnerHTML={{
+                __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.GA_MEASUREMENT_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+              }}
+            />
+          </Head>
+        ) : (
+          <Head>
+            <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+            <link rel="icon" href="/favicon.ico" sizes="any" />
+            <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+            <link rel="manifest" href="/site.webmanifest" />
+          </Head>
+        )}
         <body>
           <Main />
           <NextScript />
@@ -63,5 +54,4 @@ class MyDocument extends Document {
     )
   }
 }
-
 export default MyDocument
